f3e178dd08f374f97f356d08417afe12cf8687f8
DevMe001 <dev.me28@gmail.com>
2024-02-11 14:07:25 +0800

    added global state management for saving api with cache

---
+++ src/utils/redux/store.ts
@@ -0,0 +1,59 @@
+import {configureStore} from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { TypedUseSelectorHook } from 'react-redux';
+import { combineReducers } from "redux";
+import logger from 'redux-logger';
+import {FLUSH, PAUSE, PERSIST, Persistor, REGISTER, REHYDRATE, persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+// creates reducers
+const rootReducers = combineReducers({
+    // define api here
+})
+// create persistor key
+const peristorConfig = {
+    key:'port-management',
+    storage
+}
+
+// define persistor key and reducers
+const persistorReducer = persistReducer(peristorConfig,rootReducers);
+
+
+// create stores configurations
+const store = configureStore({
+    reducer:persistorReducer,
+    middleware:(response) => response({
+        serializableCheck:{
+            ignoredActions:[
+                FLUSH,
+                REHYDRATE,
+                PAUSE,
+                PERSIST,
+                REGISTER
+            ]
+        }
+    }).concat(
+        // define midleware
+        logger
+    ),
+    devTools:process.env.NODE_ENV != 'production'
+
+})
+
+// invokes persistor store
+export const persistorStore:Persistor = persistStore(store)
+  
+
+// create type of stores
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// create hooks for store
+export const useAppSelector:TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = ()=> useDispatch<AppDispatch>();
+
+
+export default store;
